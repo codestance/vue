4bfc9174ab2eb34421f2a0eb45c64b4f6a70b6ba
marrow00 <skrzynska.konstancja@gmail.com>
2019-10-31 10:33:55 +0100

    Vue CLI

---
+++ 2/app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Vue world"/>
+    <Message>More about that later</Message>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Message from './components/Message.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Message
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 2/app/src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <p>
+        <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.box{ background: #e3e3e3; padding:10px; border: 1px solid #e5e5e5; margin: 10px}
+</style>
