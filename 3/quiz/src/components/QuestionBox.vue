dd75e6cbd5f8017b6a4b6201319b855a333abccc
marrow00 <skrzynska.konstancja@gmail.com>
2019-11-04 15:46:50 +0100

    add loadash, styling, answer, select and shuffle

--- 3/quiz/src/components/QuestionBox.vue
+++ 3/quiz/src/components/QuestionBox.vue
@@ -8,21 +8,33 @@
 
             <hr class="my-4">
 
-            <p v-for="(answer,index) in answers" :key="index"> 
-                {{answer}}
-            </p>
-
-            <b-button variant="primary" href="#">Submit</b-button>
+            <b-list-group>
+                <b-list-group-item button
+                v-for="(answer,index) in answers"
+                :key="index"
+                @click="selectAnswer(index)"
+                :class="[selectedIndex === index ? 'selected' : '']">
+                    {{answer}}
+                </b-list-group-item>
+            </b-list-group>
+            <b-button variant="primary">Submit</b-button>
             <b-button @click ="next" variant="success" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
     props: {
         currentQuestion: Object,
         next: Function
+    },
+    data(){
+        return{
+            selectedIndex: null,
+            shuffledAnswers: []
+        }
     },
     computed: {
         answers() {
@@ -31,8 +43,49 @@ export default {
             return answers;
         }
     },
+    watch: {
+        currentQuestion: {
+            immediate: true,
+            handler() {
+                this.selectedIndex = null
+                this.shuffleAnswers()
+            }
+        //     this.selectedIndex = null;
+        //     this.shuffleAnswers();
+        }
+    },
+    methods: {
+        selectAnswer(index) {
+          this.selectedIndex = index;
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+            this.shuffledAnswers = _.shuffle(answers)
+        }
+    },
     mounted() {
         console.log(this.currentQuestion)
     }
 }
 </script>
+
+<style scoped>
+.list-group{
+    margin-bottom:15px;
+}
+.list-group-item:hover{
+    background: #eee;
+}
+.btn{
+    margin: 0 5px;
+}
+.selected{
+    background-color: lightblue;
+}
+.correct{
+    background-color:lightgreen;
+}
+.incorrect{
+    background-color: lightcoral;
+}
+</style>
