2eebd935d58c835ca01a03e8cf6b715bea5ea6b1
Unknown <skrzynska.konstancja@gmail.com>
2020-04-05 19:24:11 +0200

    adding summary

---
+++ 3/quiz/src/components/End.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <b-jumbotron>
+            <template v-slot:header>The End</template>
+            <template v-slot:lead>
+                You answered all the questions, {{numCorrect/total*100}}% of them correct.
+            </template>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['numCorrect', 'total']
+}
+</script>
