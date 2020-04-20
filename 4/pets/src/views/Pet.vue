6b6caae54c6785eb6958bacc6a79e3de10fb227f
Unknown <skrzynska.konstancja@gmail.com>
2020-04-20 21:59:23 +0200

    defining pet

--- 4/pets/src/views/Pet.vue
+++ 4/pets/src/views/Pet.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
-        Pet
+        <!-- {{$route.params.speacies}} -->
+        <!-- {{$route.params.id}} -->
+        {{pet().name}}
     </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  methods: {
+    pet () {
+      const animals = this[this.$route.params.speacies][this.$route.params.id]
+      return animals
+    }
+  }
+}
+</script>
